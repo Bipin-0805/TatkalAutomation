a086f46de38e3fa40b3923bff515f85524605e33
bipin <testaxel.01@gmail.com>
2024-05-21 11:29:51 +0530

    added login methods details

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -7,17 +7,17 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 public class TestBase {
 	
-	
-	private WebDriver driver;
-	private static Properties config;
-	private static Properties OR;
-	private static FileInputStream fis;
+	public static WebDriver driver;
+	public static Properties config;
+	public static Properties OR;
+	public static FileInputStream fis;
 	
 	@BeforeTest
 	public void Setup() throws IOException {
@@ -33,8 +33,10 @@ public class TestBase {
 		
 		if(config.getProperty("browser").equals("chrome"))
 		{
-			
-			driver= new ChromeDriver();
+			ChromeOptions options=new ChromeOptions();
+			options.addArguments("disable-notifications");
+			options.addArguments("start-maximized");
+			driver= new ChromeDriver(options);
 		}
 		else if(config.getProperty("browser").equals("Firefox")) {
 			driver= new FirefoxDriver();
@@ -42,7 +44,7 @@ public class TestBase {
 		
 		
 		
-		driver.manage().window().maximize();
+		
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Integer.parseInt(config.getProperty("implicit.wait"))));
 		driver.get(config.getProperty("baseURL"));
 		
--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -1,5 +1,6 @@
 package testcases;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import base.TestBase;
@@ -7,10 +8,25 @@ import base.TestBase;
 public class LoginTest extends TestBase{
 
 	@Test
-	public void Login() {
+	public void Login() throws InterruptedException {
 		
 	// Click Login button
+		driver.findElement(By.xpath(OR.getProperty("Login"))).click();
 		
+		
+	// Enter UserName:
+		driver.findElement(By.xpath(OR.getProperty("username"))).sendKeys(config.getProperty("Username"));
+		
+	//Enter Password:
+		driver.findElement(By.xpath(OR.getProperty("password"))).sendKeys(config.getProperty("Password"));
+		
+	//wait for captcha
+		
+		Thread.sleep(5000);
+		
+	// Click Signin Button
+		
+		driver.findElement(By.xpath(OR.getProperty("signin"))).click();
 		
 	}
 	
