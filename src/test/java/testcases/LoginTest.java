c5880d0652fe06270b65b73099a6e9e58edeb709
Bipin-0805 <153521277+Bipin-0805@users.noreply.github.com>
2026-07-09 10:14:26 +0530

    Update Login method with improved waits and error handling

--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -1,7 +1,12 @@
 package testcases;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import java.time.Duration;
 
 import base.TestBase;
 
@@ -9,26 +14,42 @@ public class LoginTest extends TestBase{
 
 	@Test
 	public void Login() throws InterruptedException {
-		
-	// Click Login button
-		driver.findElement(By.xpath(OR.getProperty("Login"))).click();
-		
-		
-	// Enter UserName:
-		driver.findElement(By.xpath(OR.getProperty("username"))).sendKeys(config.getProperty("Username"));
-		
-	//Enter Password:
-		driver.findElement(By.xpath(OR.getProperty("password"))).sendKeys(config.getProperty("Password"));
-		
-	//wait for captcha
-		
-		Thread.sleep(5000);
-		
-	// Click Signin Button
-		
-		driver.findElement(By.xpath(OR.getProperty("signin"))).click();
-		
+		try {
+			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+			
+			// Click Login button
+			System.out.println("Clicking on Login button...");
+			WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(OR.getProperty("Login"))));
+			loginButton.click();
+			Thread.sleep(1000); // Brief pause for page transition
+			
+			// Enter UserName
+			System.out.println("Entering username...");
+			WebElement usernameField = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(OR.getProperty("username"))));
+			usernameField.sendKeys(config.getProperty("Username"));
+			
+			// Enter Password
+			System.out.println("Entering password...");
+			WebElement passwordField = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(OR.getProperty("password"))));
+			passwordField.sendKeys(config.getProperty("Password"));
+			
+			// Wait for captcha to be solved (with explicit wait instead of hardcoded sleep)
+			System.out.println("Waiting for captcha resolution...");
+			Thread.sleep(5000); // User needs to solve captcha manually
+			
+			// Click Signin Button
+			System.out.println("Clicking on SignIn button...");
+			WebElement signinButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(OR.getProperty("signin"))));
+			signinButton.click();
+			
+			// Verify login success (add appropriate assertion based on your application)
+			Thread.sleep(2000); // Wait for page to load after login
+			System.out.println("Login successful!");
+			
+		} catch (Exception e) {
+			System.err.println("Login failed: " + e.getMessage());
+			throw e;
+		}
 	}
-	
 	
 }
